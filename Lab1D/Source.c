412d22bd7cc59d16827b419178bf9e363234823a
Heirat <neocron76@gmail.com>
2020-03-03 21:57:26 +0300

    Добавил проверку ввода

--- Lab1D/Source.c
+++ Lab1D/Source.c
@@ -8,9 +8,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+#define MAX_SIZE 1000
 
 int main() {
 	setlocale(LC_ALL, "RUS");
+
+	int Field[MAX_SIZE];
+	int n, m;
+	int start_r, start_c, fnsh_r, fnsh_c;
+
+	printf ("Введите количество рядов в поле\n");
+	if (scanf ("%d", &m) == 0)
+	{
+		printf ("\nНекорректный ввод\n");
+		system("pause");
+		return 1;
+	}
+	printf ("Введите количество стобцов в поле\n");
+	if (scanf ("%d", &n) == 0)
+	{
+		printf ("\nНекорректный ввод\n");
+		system("pause");
+		return 1;
+	}
+
+	printf ("Введите начальную и конечную позиции в координатах строка-столбец:\n");
+	if (scanf ("%d%d%d%d", &start_r, &start_c, &fnsh_r, &fnsh_c) != 4)
+	{
+		printf ("\nНекорректный ввод\n");
+		system("pause");
+		return 1;
+	}
 
 
 
